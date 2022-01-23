a4fd29c525e3bd35f4a571a93bcfc7200fe5a2f9
shubhamku044 <shubhamku044@gmail.com>
2022-01-23 23:05:28 +0530

    added social media links

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,6 +1,7 @@
 import Typed from 'react-typed';
 import styles from '../styles/components/hero.module.scss';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const Hero = () => {
 	return (
@@ -9,6 +10,68 @@ const Hero = () => {
 				<div className={`${styles['hero__container-left']}`}>
 					<h2>I am shubham</h2>
 					<p>I am a frontend developer</p>
+					<ul className={`${styles['hero__container-icons']}`}>
+						<li>
+							<Link href="https://github.com/shubhamku044">
+								<a target={'_blank'}>
+									<Image
+										src="/icons/github.svg"
+										objectFit="contain"
+										width={28}
+										height={28}
+									/>
+								</a>
+							</Link>
+						</li>
+						<li>
+							<Link href="https://www.linkedin.com/in/shubhamku044/">
+								<a target={'_blank'}>
+									<Image
+										src="/icons/linkedin.svg"
+										objectFit="contain"
+										width={28}
+										height={28}
+									/>
+								</a>
+							</Link>
+						</li>
+						<li>
+							<Link href="https://twitter.com/shubhamku044">
+								<a target={'_blank'}>
+									<Image
+										src="/icons/twitter.svg"
+										objectFit="contain"
+										width={28}
+										height={28}
+									/>
+								</a>
+							</Link>
+						</li>
+						<li>
+							<Link href="https://www.instagram.com/thelegitprogrammer/">
+								<a target={'_blank'}>
+									<Image
+										src="/icons/instagram.svg"
+										objectFit="contain"
+										width={28}
+										height={28}
+									/>
+								</a>
+							</Link>
+						</li>
+						<li>
+							<Link href="https://wa.link/dopzjz">
+								<a target={'_blank'}>
+									<Image
+										src="/icons/whatsapp.svg"
+										objectFit="contain"
+										width={28}
+										height={28}
+									/>
+								</a>
+							</Link>
+						</li>
+					</ul>
 				</div>
 				<div className={`${styles['hero__container-right']}`}>
 					<div
