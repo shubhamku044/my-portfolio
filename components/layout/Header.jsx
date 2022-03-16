49ba952bc27dcd242011a2d674ed63693a1d25e5
Shubham Sharma <shubhamku044@gmail.com>
2022-03-16 20:40:56 +0530

    completed navbar for mobile

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,8 +1,10 @@
 import styles from '../../styles/layout/Header.module.scss';
 import { useState, useEffect } from 'react';
+import { navBarState } from '../../atoms/navbarState';
+import { useRecoilState } from 'recoil';
 
 const Header = () => {
-  const [openNav, setOpenNav] = useState(false);
+  const [openNav, setOpenNav] = useRecoilState(navBarState);
   const [position, setPosition] = useState(window.pageYOffset);
   const [visible, setVisible] = useState(true);
 
@@ -20,39 +22,63 @@ const Header = () => {
   });
 
   return (
-    <div
-      className={`${styles.container} ${
-        visible ? styles['container-visible'] : styles['container-hidden']
-      }`}
-      style={
-        window.pageYOffset > 50
-          ? { boxShadow: '0 3px 10px rgb(0 0 0 / 0.2)' }
-          : {}
-      }
-    >
-      <nav className={styles.container__main}>
-        <h1 className={styles['container__main-left']}>SS</h1>
-        <div className={styles['container__main-right']}>
-          <ul>
-            <li>About</li>
-            <li>Projects</li>
-            <li>Work</li>
-            <li>Blogs</li>
-          </ul>
-          <div className={styles['container__main-button']}>Contact</div>
-          <div
-            className={`${styles['container__main-hamburger']} ${
-              openNav ? styles['container__main-hamburger-active'] : ''
-            }`}
-            onClick={() => setOpenNav(!openNav)}
-          >
-            <span className={styles['container__main-hamburger-1']}></span>
-            <span className={styles['container__main-hamburger-2']}></span>
-            <span className={styles['container__main-hamburger-3']}></span>
+    <>
+      <div
+        className={`${styles.container} ${
+          visible ? styles['container-visible'] : styles['container-hidden']
+        }`}
+        style={
+          window.pageYOffset > 50
+            ? { boxShadow: '0 3px 10px rgb(0 0 0 / 0.2)' }
+            : {}
+        }
+      >
+        <nav className={styles.container__main}>
+          <h1 className={styles['container__main-left']}>SS</h1>
+          <div className={styles['container__main-right']}>
+            <ul>
+              <li>About</li>
+              <li>Projects</li>
+              <li>Work</li>
+              <li>Blogs</li>
+            </ul>
+            <div className={styles['container__main-button']}>Contact</div>
+            <div
+              className={`${styles['container__main-hamburger']} ${
+                openNav ? styles['container__main-hamburger-active'] : ''
+              }`}
+              onClick={() => setOpenNav(!openNav)}
+            >
+              <span className={styles['container__main-hamburger-1']}></span>
+              <span className={styles['container__main-hamburger-2']}></span>
+              <span className={styles['container__main-hamburger-3']}></span>
+            </div>
           </div>
-        </div>
-      </nav>
-    </div>
+        </nav>
+      </div>
+      <div className={styles.container__aside}>
+        <aside
+          className={`${styles['container__aside-main']} ${
+            openNav ? styles['container__aside-main-active'] : ''
+          }`}
+        >
+          <nav>
+            <ol>
+              <li>About</li>
+              <li>Projects</li>
+              <li>Work</li>
+              <li>Blogs</li>
+            </ol>
+          </nav>
+        </aside>
+        <div
+          className={`${styles['container__aside-overlay']} ${
+            openNav ? styles['container__aside-overlay-active'] : ''
+          }`}
+          onClick={() => setOpenNav(!openNav)}
+        />
+      </div>
+    </>
   );
 };
 
--- pages/index.jsx
+++ pages/index.jsx
@@ -6,14 +6,28 @@ import Footer from '../components/layout/Footer';
 import dynamic from 'next/dynamic';
 import Social from '../components/layout/Social';
 import Mail from '../components/layout/Mail';
+import { navBarState } from '../atoms/navbarState';
+import { useRecoilState } from 'recoil';
 
 const Header = dynamic(() => import('../components/layout/Header'), {
   ssr: false,
 });
 
 export default function Home() {
+  const [openNav, setOpenNav] = useRecoilState(navBarState);
+
   return (
-    <div className={styles.container}>
+    <div
+      className={styles.container}
+      style={
+        openNav
+          ? {
+              position: 'fixed',
+              overflow: 'hidden',
+            }
+          : {}
+      }
+    >
       <Head>
         <title>Shubham Sharma</title>
         <link rel="icon" href="/favicon.ico" />
