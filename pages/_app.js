83f9b4549caab4ff3a3863687c95777aff0c4f30
Shubham Sharma <shubhamku044@gmail.com>
2022-08-23 02:16:49 +0530

    added the g-analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,22 @@
 import { RecoilRoot } from 'recoil';
 import '../styles/globals.scss';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script
+        strategy="lazyOnload"
+        src={'https://www.googletagmanager.com/gtag/js?id=G-HF2NQPTQ7W'}
+      />
+      <Script strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-HF2NQPTQ7W');
+          `}
+      </Script>
       <RecoilRoot>
         <Component {...pageProps} />
       </RecoilRoot>
