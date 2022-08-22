8ec1c33cf077eb5827aab0fabad198e719e91dd3
Shubham Sharma <shubhamku044@gmail.com>
2022-08-23 03:02:15 +0530

    updated seo for website

--- pages/index.jsx
+++ pages/index.jsx
@@ -32,6 +32,27 @@ export default function Home() {
     >
       <Head>
         <title>Shubham Sharma</title>
+        <meta
+          name="description"
+          content="Hello, I am Shubham Sharma, a full-stack developer with some knowledge of web3 also, a freelancer."
+        />
+        <meta property="og:title" content="Shubham Sharma" />
+        <meta
+          property="og:description"
+          content="Hello, I am Shubham Sharma, a full-stack developer with some knowledge of web3 also, a freelancer."
+        />
+        <meta property="og:url" content="https://www.shubhams.dev/" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:creator" content="@shubhamku044" />
+        <meta
+          name="twitter:description"
+          content="Hello, I am Shubham Sharma, a full-stack developer with some knowledge of web3 also, a freelancer."
+        />
+        <meta
+          name="keywords"
+          content="shubhamku044, shubham sharma, shubham kumar, shubham dev, shubhams, shubhams.dev"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
