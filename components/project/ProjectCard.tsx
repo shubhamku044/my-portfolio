ec955d61a957193a4176dfa03f6be25ce3ba622d
Shubham Sharma <shubhamku044@gmail.com>
2022-09-21 18:42:27 +0530

    created project card

--- components/project/ProjectCard.tsx
+++ components/project/ProjectCard.tsx
@@ -1,17 +1,20 @@
 import styles from '@styles/components/projects/projectCard.module.scss';
 import type { NextPage } from 'next';
-import Image from 'next/future/image';
+import Image from 'next/image';
 
 const ProjectCard: NextPage = () => {
   return (
     <div className={styles.container}>
-      <Image
-        src={'/yaelabs.gif'}
-        width={800}
-        height={500}
-        loading={'lazy'}
-        alt={'Yaelabs'}
-      />
+      <div className={styles.container__img}>
+        <Image
+          src={'/yaelabs.gif'}
+          layout={'fill'}
+          loading={'lazy'}
+          alt={'Yaelabs'}
+          objectFit={'contain'}
+        />
+      </div>
+      <div className={styles.container__desc}></div>
     </div>
   );
 };
