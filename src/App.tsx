de08a3f8064212b206b19f8982af2437d16d1ed5
Shubham Sharma <shubhamku044@gmail.com>
2023-02-25 02:18:06 +0530

    fix: padding

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="px-4 py-4">
       <Router>
         <Header />
-        <main className="max-w-3xl px-4 sm:px-10 mx-auto mt-24 mb-16 min-h-[90vh]">
+        <main className="max-w-3xl px-2 sm:px-10 mx-auto mt-24 mb-16 min-h-[90vh]">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/blog" element={<Blog />} />
