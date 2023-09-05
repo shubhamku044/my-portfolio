f0962f7098e787cf8b19d2b2d3db26c5064520ad
Shubham Sharma <shubhamku044@gmail.com>
2023-09-05 17:30:31 +0530

    fix: deployment issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -107,7 +107,7 @@ const styles = {
 
 const Home = () => (
   <div className='space-y-10'>
-    <div className='space-y-4'>
+    {/* <div className='space-y-4'>
       <h1 className='text-4xl font-bold'>Hey, I&apos;m Shubham 👋</h1>
       <p className={styles.desc}>
         I&apos;m a fullstack developer, mainly working with NodeJS applications.
@@ -177,7 +177,7 @@ const Home = () => (
     <div className='space-y-4'>
       <h4 className={styles.subHeading}>Experience 🏢</h4>
       <Experience />
-    </div>
+    </div> */}
   </div>
 );
 
