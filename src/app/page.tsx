7f120e29bd5917497d9834f93c22d9fdef7b6a3f
Shubham Sharma <shubhamku044@gmail.com>
2023-11-05 22:33:58 +0530

    content update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-'use client';
 import React, { ReactNode } from 'react';
 import {
   SiTypescript,
@@ -103,78 +102,81 @@ const styles = {
   desc: 'text-gray-700 dark:text-gray-300',
 };
 
-const Home = () => (
-  <div className="space-y-10">
-    <div className="space-y-4">
-      <h1 className="text-4xl font-bold">Hey, I&apos;m Shubham 👋</h1>
-      <p className={styles.desc}>
-        I&apos;m a fullstack developer, mainly working with NodeJS applications.
-        I like to work on discord bot&apos;s, using discord.js, or working on sites
-        for myself, my friends or for a project I am working on! I mainly use TypeScript,
-        I have been using Lua more lately for another position I have within a gaming community.
-      </p>
+const Home = () => {
+  return (
+    <div className="space-y-10">
+      <div className="space-y-4">
+        <h1 className="text-4xl font-bold">Hey, I&apos;m Shubham 👋</h1>
+        <p className={styles.desc}>
+          As a passionate full-stack developer, I thrive on crafting dynamic web applications using the
+          power of React and Express. My journey in software development has led me to explore the exciting
+          world of Rust, where I&apos;m continuously expanding my skill set to create robust, high-performance
+          solutions. I&apos;m committed to delivering exceptional results in every project, blending creativity with
+          technical expertise.
+        </p>
+      </div>
+      <div className="space-y-4">
+        <h4 className={styles.subHeading}>Technologies 💻</h4>
+        <p className={styles.desc}>
+          I employ a diverse array of tools and technologies to enhance my development workflow and elevate the caliber
+          of my code and projects. The following is a compilation of the languages and technologies with which I have
+          gained proficiency through prior experience and actively utilize in my current work.
+        </p>
+        <ul className="flex w-full flex-wrap items-center justify-center gap-3 border-2 p-2 dark:border-gray-900 sm:gap-5">
+          {
+            techStack.map(({ icon }, index) => (
+              <li key={index}>
+                <span>
+                  {icon}
+                </span>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+      <div className="space-y-4">
+        <h4 className="text-3xl font-semibold">Projects 🛠️</h4>
+        <p className={styles.desc}>
+          During my leisure hours, I take pleasure in crafting open-source projects on GitHub, both as a means of
+          continuous learning and a platform for sharing my expertise. Here, I present a selection of my most
+          well-received repositories.
+        </p>
+        <ul className="grid grid-cols-1 gap-2 md:grid-cols-2">
+          <li>
+            <ProjectCard
+              name="Discord Bot"
+              description="A discord bot I created for my friends and I to use in our discord server."
+              githubLink=""
+              liveLink=""
+              techStack={[<SiTypescript className="h-5 w-5" key={1} />, <SiTailwindcss className="h-5 w-5" key={2} />]}
+            />
+          </li>
+          <li>
+            <ProjectCard
+              name="Discord Bot"
+              description="A discord bot I created for my friends and I to use in our discord server."
+              githubLink=""
+              liveLink=""
+              techStack={[<SiTypescript className="h-5 w-5" key={1} />, <SiTailwindcss className="h-5 w-5" key={2} />]}
+            />
+          </li>
+          <li>
+            <ProjectCard
+              name="Discord Bot"
+              description="A discord bot I created for my friends and I to use in our discord server."
+              githubLink=""
+              liveLink=""
+              techStack={[<SiTypescript className="h-5 w-5" key={1} />, <SiTailwindcss className="h-5 w-5" key={2} />]}
+            />
+          </li>
+        </ul>
+      </div>
+      <div className="space-y-4">
+        <h4 className={styles.subHeading}>Experience 🏢</h4>
+        <Experience />
+      </div>
     </div>
-    <div className="space-y-4">
-      <h4 className={styles.subHeading}>Technologies 💻</h4>
-      <p className={styles.desc}>
-        I use a variety of tools to streamline my development process and increase the quality of
-        both my code, and my projects. Below is a list of technologies and languages I&apos;ve
-        had experience with in the past, or use currently.
-      </p>
-      <ul className="flex w-full flex-wrap items-center justify-center gap-3 border-2 p-2 dark:border-gray-900 sm:gap-5">
-        {
-          techStack.map(({ icon, name }, index) => (
-            <li key={index}>
-              <span>
-                {icon}
-              </span>
-            </li>
-          ))
-        }
-      </ul>
-    </div>
-    <div className="space-y-4">
-      <h4 className="text-3xl font-semibold">Projects 🛠️</h4>
-      <p className={styles.desc}>
-        In my free time, I enjoy creating open source projects on GitHub, so I can learn from others
-        and showcase what I know. In total, all of my open sourced projects have earnt me 10 stars
-        on GitHub, and 4 forks. Below are some of my most popular repositories.
-      </p>
-      <ul className="grid grid-cols-1 gap-2 md:grid-cols-2">
-        <li>
-          <ProjectCard
-            name="Discord Bot"
-            description="A discord bot I created for my friends and I to use in our discord server."
-            githubLink=""
-            liveLink=""
-            techStack={[<SiTypescript className="h-5 w-5" key={1} />, <SiTailwindcss className="h-5 w-5" key={2} />]}
-          />
-        </li>
-        <li>
-          <ProjectCard
-            name="Discord Bot"
-            description="A discord bot I created for my friends and I to use in our discord server."
-            githubLink=""
-            liveLink=""
-            techStack={[<SiTypescript className="h-5 w-5" key={1} />, <SiTailwindcss className="h-5 w-5" key={2} />]}
-          />
-        </li>
-        <li>
-          <ProjectCard
-            name="Discord Bot"
-            description="A discord bot I created for my friends and I to use in our discord server."
-            githubLink=""
-            liveLink=""
-            techStack={[<SiTypescript className="h-5 w-5" key={1} />, <SiTailwindcss className="h-5 w-5" key={2} />]}
-          />
-        </li>
-      </ul>
-    </div>
-    <div className="space-y-4">
-      <h4 className={styles.subHeading}>Experience 🏢</h4>
-      <Experience />
-    </div>
-  </div>
-);
+  );
+};
 
 export default Home;
